9ebde95bfefc80fed22188984b41c0e34864220b
Santhosh-2711 <130351694+Santhosh-2711@users.noreply.github.com>
2025-06-05 01:21:47 +0530

    Add files via upload

---
+++ User/src/main/java/com/example/carwash/entity/User.java
@@ -0,0 +1,106 @@
+package com.example.carwash.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+   
+	private String address;
+	private int age;
+    
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // CUSTOMER or WASHER
+
+    
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	 public String getPhoneNumber() {
+			return phoneNumber;
+		}
+
+		public void setPhoneNumber(String phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	// Constructors
+    public User() {}
+
+	public User(Long id, String username, String password, String email, String phoneNumber, String address, int age,
+			Role role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.age = age;
+		this.role = role;
+	}
+
+    // Getters & Setters
+}
